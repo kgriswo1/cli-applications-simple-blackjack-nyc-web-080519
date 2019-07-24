f43304c5daaa5b325b4bc4b11ea04718c1d7d0ab
Kunxi Griswold <kgriswo1@binghamton.edu>
2019-07-24 06:59:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,12 +36,12 @@ end
 def hit?(num)
   prompt_user
   answer = get_user_input
-  # if answer == "h"
-  #   new_card = deal_card
-  #   num += new_card
-  # elsif !(answer == "s" || answer == "h")
-  #   invalid_command
-  # end
+  if answer == "h"
+    new_card = deal_card
+    num += new_card
+  elsif !(answer == "s" || answer == "h")
+    invalid_command
+  end
   # display_card_total(num)
 end
 
